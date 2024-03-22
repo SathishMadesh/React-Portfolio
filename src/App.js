442fd0a12cb2f1954fcdd501b962c9993b2146e4
SathishMadesh <s747272946@gmail.com>
2024-03-22 20:04:41 +0530

    update

--- src/App.js
+++ src/App.js
@@ -2,10 +2,14 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import Navbar from './components/Navbar';
+import Header from './components/Header';
 
 function App() {
   return (
-    <Navbar/>
+    <>
+      <Navbar />
+      <Header />
+    </>
   );
 }
 
---
+++ src/components/Header.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { ReactTyped } from 'react-typed';
+
+function Header() {
+  return (
+    <div className="header">
+      <div className="main-info">
+        <h1>Web Developer</h1>
+        <ReactTyped className="typed-text"
+        strings={["Front End Development", "Web Development"]}
+        typeSpeed = {40}
+        backSpeed = {60}
+        loop/>
+        <a href="#" className="btn-main">Download CV</a>
+      </div>
+    </div>
+  )
+}
+
+export default Header
