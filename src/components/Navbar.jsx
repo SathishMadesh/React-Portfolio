a74fe3c364bbed9ae60d168b75a4dcdb6f3cdb99
SathishMadesh <s747272946@gmail.com>
2024-03-22 18:46:57 +0530

    update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -15,7 +15,16 @@ const Navbar = () => {
                             <a className="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="#">Link</a>
+                            <a className="nav-link" href="#">Introduction</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Skills</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Projects</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Contact</a>
                         </li>
                     </ul>
                 </div>
