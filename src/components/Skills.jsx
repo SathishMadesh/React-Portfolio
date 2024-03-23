2da869ec4975f68703f713af44251d8d6c18f0a9
SathishMadesh <s747272946@gmail.com>
2024-03-23 16:33:11 +0530

    ipdate

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from 'react';
+import skills from './data/skills.json';
 
 function Skills() {
   return (
     <>
-      <div className="skills-container"></div>
+      <div className="skills-container">
+        <h1>Skills</h1>
+        {skills.map((data)=>(
+            <>
+            <div className="items" key={data.key} >
+                <div className="item" >
+                    <img src={data.image} alt={data.title} />
+                    <h3>{data.title}</h3>
+                </div>
+            </div>
+            </>
+        ))}
+      </div>
     </>
   )
 }
