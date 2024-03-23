e6108e4c8e27f9ad071253b19c94678c8fde6f39
SathishMadesh <s747272946@gmail.com>
2024-03-23 17:28:21 +0530

    update

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -2,23 +2,23 @@ import React from 'react';
 import skills from './data/skills.json';
 
 function Skills() {
-  return (
-    <>
-      <div className="skills-container">
-        <h1>Skills</h1>
-        {skills.map((data)=>(
-            <>
-            <div className="items" key={data.key} >
-                <div className="item" >
-                    <img src={data.image} alt={data.title} />
-                    <h3>{data.title}</h3>
+    return (
+        <div className="outer-container bg-dark">
+            <div className="container">
+                <div className="skills-container row">
+                    <h1 className="col-12">Skills</h1>
+                    {skills.map((data) => (
+                        <div className="col-lg-4 col-md-6 col-sm-12 items">
+                            <div className="item" key={data.key}>
+                                <img src={data.image} alt={data.title} className="img-fluid" />
+                                <h3>{data.title}</h3>
+                            </div>
+                        </div>
+                    ))}
                 </div>
             </div>
-            </>
-        ))}
-      </div>
-    </>
-  )
+        </div>
+    )
 }
 
 export default Skills
