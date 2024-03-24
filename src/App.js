3f099736adc140f7acca3f1aa51bbd986f4efcae
SathishMadesh <s747272946@gmail.com>
2024-03-24 13:33:18 +0530

    update

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Navbar from './components/Navbar';
 import Header from './components/Header';
 import AboutMe from './components/AboutMe';
 import Skills from './components/Skills';
+import Projects from './components/Projects';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
       <Header />
       <AboutMe />
       <Skills />
+      <Projects />
     </>
   );
 }
---
+++ src/components/Projects.jsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import Axios from 'axios';
+import { useState, useEffect } from 'react';
+
+function Projects() {
+    const [projects, setProjects] = useState([]);
+
+    useEffect(() => {
+        Axios.get('https://sathishmadesh.github.io/projects-api/projects.json')
+            .then((response) => {
+                setProjects(response.data);
+            });
+    }, [])
+
+    return (
+        <>
+            <div className="project-container bg-dark py-3 ">
+                
+                <div className="container">
+                    <div className="row ">
+                    <h1 className="col-12 proj-title">Projects</h1>
+                        {projects.map((data) => (<>
+                            <div key={data.id} className="col-lg-4 col-md-6 col-sm-12 my-3 d-flex justify-content-center align-content-center">
+                                <div className="card text-light" style={{width: "auto", border:"2px solid orange", borderRadius:"20px", boxShadow:"5px 5px 10px 0px orange" }}>
+                                    <div className="img d-flex justify-content-center align-content-center p-3">
+                                    <img src={data.image} className="card-img-top" alt="..." 
+                                    style={{
+                                        // width: "250px",
+                                        height: "220px",
+                                        border: "1px solid orange",
+                                        borderRadius: "10px"
+                                    }}/>
+                                    </div>
+                                        <div className="card-body text-center">
+                                            <h5 className="card-title">{data.title}</h5>
+                                            <p className="card-text">{data.discription}</p>
+                                            <a href={data.gitlink} className="btn btn-primary">View Source Code</a>
+                                        </div>
+                                </div>
+                            </div>
+                        </>))}
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Projects
